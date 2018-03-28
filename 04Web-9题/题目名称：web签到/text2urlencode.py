5f8017421fdc22c4e042e5a7030b062ff68cc493
Jas502n <jas502n@gmail.com>
2018-03-28 21:45:10 +0800

    update

--- 04Web-9题/题目名称：web签到/text2urlencode.py
+++ 04Web-9题/题目名称：web签到/text2urlencode.py
@@ -6,8 +6,13 @@ file1 = open("message1.bin", "rb")
 file2 = open("message2.bin", "rb")
 res1 = file1.read()
 res2 = file2.read()
+
 s1 = urllib.quote(res1)
 s2 = urllib.quote(res2)
 file1.close()
 file2.close()
+
 print 'param1=%s'% s1 +'&'+'param2=%s'% s2
+
+
+
